4e8dfd98e55a390ee2cd86e77701fe9039b0ecad
Miles Vendetti-Houser <milesvendetti@gmail.com>
2019-07-12 16:30:20 -0400

    Adds PlayingCard class

---
+++ PlayingCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfWar
+{
+    class PlayingCard
+    {
+        public enum SUITE
+        {
+            DIAMONDS,
+            SPADES,
+            CLUBS,
+            HEARTS
+        };
+
+        public enum RANK {
+            
+            TWO = 2,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING,
+            ACE
+        };
+
+        public SUITE Suite { get; set; }
+        public RANK Rank { get; set; }
+
+    }
+}
