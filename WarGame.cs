06692093fb572e26cd0f285a4bdfad99bc92f8d9
Miles Vendetti-Houser <milesvendetti@gmail.com>
2019-07-13 21:09:48 -0400

    Adds WarGame class

---
+++ WarGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfWar
+{
+    public class WarGame
+    {
+        WarPlayer player1 = null;
+        WarPlayer player2 = null;
+        
+        public WarGame()
+        {
+            player1 = new WarPlayer();
+            player2 = new WarPlayer();
+        }
+
+        public void DealCards()
+        {
+            DeckOfCards tmpDeck = new DeckOfCards(true);
+            tmpDeck.Shuffle();
+
+            for (int i = 0; i < DeckOfCards.STANDARD_DECK_SIZE; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    player1.AddCardToDeck(tmpDeck.DealCard());
+                }
+                else
+                {
+                    player2.AddCardToDeck(tmpDeck.DealCard());
+                }
+            }
+        }
+
+
+    }
+}
