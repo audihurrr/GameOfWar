42bca2180eac7cec8248d115b154183b81b76253
Miles Vendetti-Houser <milesvendetti@gmail.com>
2019-07-13 21:14:24 -0400

    Updates DeckOfCards with Shuffle, initialization, adding and removing card from deck

--- DeckOfCards.cs
+++ DeckOfCards.cs
@@ -1,7 +1,73 @@
-﻿namespace GameOfWar
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfWar
 {
-    public class DeckOfCards :  PlayingCard
-    {
-        
+    public class DeckOfCards
+    { 
+        public const int STANDARD_DECK_SIZE = 52;
+
+        private List<PlayingCard> deck = null;
+
+        public int Count
+        {
+            get
+            {
+                return deck.Count;
+            }
+        }
+
+        public DeckOfCards(bool fillDeck)
+        {
+            deck = new List<PlayingCard>();
+            if (fillDeck)
+            {
+                InitializeDeck();
+            }
+        }
+
+        private void InitializeDeck()
+        {
+            foreach (PlayingCard.SUITE s in Enum.GetValues(typeof(PlayingCard.SUITE)))
+            {
+                foreach (PlayingCard.RANK r in Enum.GetValues(typeof(PlayingCard.RANK)))
+                {
+                    deck.Add(new PlayingCard() { Suite = s, Rank = r });      
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = deck.Count;
+
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                PlayingCard tmp = deck[k];
+                deck[k] = deck[n];
+                deck[n] = tmp;
+            }
+        }
+
+        public void AddCard(PlayingCard newCard)
+        {
+            deck.Add(newCard);
+        }
+
+        public PlayingCard DealCard()
+        {
+            PlayingCard dealtCard = null;
+            if (deck.Count > 0)
+            {
+                dealtCard = deck[0];
+                deck.RemoveAt(0);
+            }
+            return dealtCard;
+        }
+
     }
+
 }
